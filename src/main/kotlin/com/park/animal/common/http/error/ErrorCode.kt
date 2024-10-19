43d6d326e7ff6db82d3f3d61eb59a4fbc1b93a24
PARKPARKWOO <wy9295@naver.com>
2024-10-19 16:12:40 +0900

    feat: Update custom exception for http status

--- src/main/kotlin/com/park/animal/common/http/error/ErrorCode.kt
+++ src/main/kotlin/com/park/animal/common/http/error/ErrorCode.kt
@@ -1,25 +1,29 @@
 package com.park.animal.common.http.error
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+
 enum class ErrorCode(
     val message: String,
+    val httpCode: HttpStatusCode,
 ) {
-    NO_BEARER_TOKEN(""),
-    EXPIRED_JWT(""),
-    PARSE_JWT_FAILED(""),
-    REISSUE_JWT_TOKEN_FAILURE(""),
+    NO_BEARER_TOKEN("", HttpStatus.UNAUTHORIZED),
+    EXPIRED_JWT("", HttpStatus.UNAUTHORIZED),
+    PARSE_JWT_FAILED("", HttpStatus.BAD_REQUEST),
+    REISSUE_JWT_TOKEN_FAILURE("", HttpStatus.UNAUTHORIZED),
 
-    UNAUTHORIZED("작업을 수행할 권한이 없습니다."),
+    FORBIDDEN("작업을 수행할 권한이 없습니다.", HttpStatus.FORBIDDEN),
 
-    AUTHENTICATION_RESOLVER_ERROR(""),
-    NOT_FOUND_REQUEST(""),
+    AUTHENTICATION_RESOLVER_ERROR("", HttpStatus.INTERNAL_SERVER_ERROR),
+    NOT_FOUND_REQUEST("", HttpStatus.BAD_REQUEST),
 
     // user
-    NOT_FOUND_USER("user 를 찾을 수 없습니다"),
+    NOT_FOUND_USER("user 를 찾을 수 없습니다", HttpStatus.BAD_REQUEST),
 
     // post
-    NOT_FOUND_POST("게시글 상세조회 실패"),
-    FAILURE_UPLOAD_IMAGE("이미지 업로드하는데 실패했습니다."),
-    NOT_FOUND_POST_IMAGE("게시글 이미지 조회 실패"),
+    NOT_FOUND_POST("게시글 상세조회 실패", HttpStatus.BAD_REQUEST),
+    FAILURE_UPLOAD_IMAGE("이미지 업로드하는데 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    NOT_FOUND_POST_IMAGE("게시글 이미지 조회 실패", HttpStatus.BAD_REQUEST),
 
-    UNKNOWN_ERROR("알 수 없는 에러"),
+    UNKNOWN_ERROR("알 수 없는 에러", HttpStatus.INTERNAL_SERVER_ERROR),
 }
