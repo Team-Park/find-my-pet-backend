44907aac5f6c9f6da463f04001d1d3ae20f12559
PARKPARKWOO <wy9295@naver.com>
2025-05-01 00:51:26 +0900

    feat: Add GitHub Actions workflow for toolkit version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/PARKPARKWOO/common-module")
+            credentials {
+                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_USERNAME")
+                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
 rootProject.name = "animal"
